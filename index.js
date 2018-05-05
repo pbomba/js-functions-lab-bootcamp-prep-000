04fff0bd1271e65e312e433425db57af645b0bf7
Paul Bomba` <paul@paulbomba.com>
2018-05-05 17:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // write your code below!
 
-Function happyHolidays(){
+Function happyHolidays () {
   return "Happy holidays!";
 }
 
-Function happyHolidaysTo(name){
+Function happyHolidaysTo (name) {
   return `Happy holidays ${name}`;
+}
+
+Function happyHolidayTo (name, holiday) {
+  
 }
