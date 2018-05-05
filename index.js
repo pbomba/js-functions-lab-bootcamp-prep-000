a9c03db9eba783d5e7c67195553382b9423f7ba7
Paul Bomba` <paul@paulbomba.com>
2018-05-05 17:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function happyHolidayTo (name, holiday) {
 }
 
 function holidayCountdown (holiday, days) {
-  return `it's ${days} days until ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`;
   
 }
